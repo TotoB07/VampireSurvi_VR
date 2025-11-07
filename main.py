5de4e80401809394d810003e3746781fffb62b7c
TotoB07 <thomas63110@gmail.com>
2025-11-07 15:23:25 +0100

    ajout crosshair + camera

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFile 
 from panda3d.core import DirectionalLight, AmbientLight
+from panda3d.core import TransparencyAttrib
+from direct.gui.OnscreenImage import OnscreenImage
 
 loadPrcFile('settings.prc')
 
 class MyGame(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
         self.loadModels()
         self.setupLights()
+        self.generateTerrain()
+        self.setupCamera()
+
+    def setupCamera(self):
+        self.disableMouse()
+        self.camera.setPos(0, 0, 3)
+        crosshairs = OnscreenImage(
+            image='crosshairs.png', 
+            pos=(0, 0, 0), 
+            scale=0.05
+        )
+        crosshairs.setTransparency(TransparencyAttrib.MAlpha)
+         
+
+    def generateTerrain(self):
+        for z in range(10):
+            for y in range(20):
+                for x in range(20):
+                    newBlockNode = render.attachNewNode('new-block_placeholder')
+
+                    newBlockNode.setPos(x*2 -20, y*2 -20, -z *2)
+
+                    if z == 0:
+                        self.grassBlock.instanceTo(newBlockNode)
+                    elif z < 4:
+                        self.dirtBlock.instanceTo(newBlockNode)
+                    else:
+                        self.stoneBlock.instanceTo(newBlockNode)
 
     def loadModels(self):
         self.grassBlock = loader.loadModel('grass-block.glb')
-        self.grassBlock.reparentTo(render)
-
         self.dirtBlock = loader.loadModel('dirt-block.glb')
-        self.dirtBlock.setPos(0, 2, 0)
-        self.dirtBlock.reparentTo(render)
-
         self.stoneBlock = loader.loadModel('stone-block.glb')
-        self.stoneBlock.setPos(0, 4, 0)
-        self.stoneBlock.reparentTo(render)
-
         self.sandBlock = loader.loadModel('sand-block.glb')
-        self.sandBlock.setPos(0, 6, 0)
-        self.sandBlock.reparentTo(render)
 
     def setupLights(self):
         mainLight = DirectionalLight('mainLight')
