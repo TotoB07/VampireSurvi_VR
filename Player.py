f4b972f25de01803265d7d09e478e9e078924b90
TotoB07 <thomas63110@gmail.com>
2025-11-08 17:15:44 +0100

    restructuration code

---
+++ Player.py
@@ -0,0 +1,194 @@
+from math import pi, sin, cos
+from panda3d.core import CollisionNode, CollisionSphere, CollisionRay, BitMask32
+from panda3d.core import TransparencyAttrib, WindowProperties
+from direct.gui.OnscreenImage import OnscreenImage
+
+def degToRad(deg):
+    return deg * (pi / 180)
+
+class Player():
+    def __init__(self, game):
+        self.game = game  # référence vers la classe principale
+        
+        # Physique
+        self.zVel = 0.0
+        self.gravity = -20.0
+        self.jumpSpeed = 7.0
+        self.onGround = False
+        self.moveSpeed = 10
+        
+        # Contrôles
+        self.setupControls()
+        self.setupCamera()
+        
+        # État de la souris
+        self.cameraSwingActivated = False
+        self.cameraSwingFactor = 10
+
+    def setupControls(self):
+        self.keyMap = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False,
+            "up": False,
+            "down": False
+        }
+        
+        # Binding des touches
+        self.game.accept("escape", self.releaseMouse)
+        self.game.accept("mouse1", self.captureMouse)
+        self.game.accept("z", self.updateKeyMap, ["forward", True])
+        self.game.accept("z-up", self.updateKeyMap, ["forward", False])
+        self.game.accept("s", self.updateKeyMap, ["backward", True])
+        self.game.accept("s-up", self.updateKeyMap, ["backward", False])
+        self.game.accept("q", self.updateKeyMap, ["left", True])
+        self.game.accept("q-up", self.updateKeyMap, ["left", False])
+        self.game.accept("d", self.updateKeyMap, ["right", True])
+        self.game.accept("d-up", self.updateKeyMap, ["right", False])
+        self.game.accept("space", self.updateKeyMap, ["up", True])
+        self.game.accept("space-up", self.updateKeyMap, ["up", False])
+        self.game.accept("lshift", self.updateKeyMap, ["down", True])
+        self.game.accept("lshift-up", self.updateKeyMap, ["down", False])
+
+    def setupCamera(self):
+        self.game.disableMouse()
+        self.game.camera.setPos(0, 0, 3)
+        
+        # Crosshair
+        self.crosshairs = OnscreenImage(
+            image='img/crosshairs.png', 
+            pos=(0, 0, 0), 
+            scale=0.05
+        )
+        self.crosshairs.setTransparency(TransparencyAttrib.MAlpha)
+
+        # Collisions
+        self.setupCollisions()
+
+    def setupCollisions(self):
+        # Collider joueur
+        playerNode = CollisionNode('player-collider')
+        playerNode.addSolid(CollisionSphere(0, 0, 0, 0.5))
+        playerNode.setFromCollideMask(self.game.worldMask)
+        playerNode.setIntoCollideMask(BitMask32.allOff())
+        self.playerCollider = self.game.camera.attachNewNode(playerNode)
+        self.game.cTrav.addCollider(self.playerCollider, self.game.pusher)
+        self.game.pusher.addCollider(self.playerCollider, self.game.camera)
+
+        # Rayon sol
+        floorRay = CollisionRay(0, 0, 0, 0, 0, -1)
+        floorNode = CollisionNode('floor-ray')
+        floorNode.addSolid(floorRay)
+        floorNode.setFromCollideMask(self.game.worldMask)
+        floorNode.setIntoCollideMask(BitMask32.allOff())
+        self.floorRayNP = self.game.camera.attachNewNode(floorNode)
+        self.floorQueue = self.game.floorQueue
+        self.game.cTrav.addCollider(self.floorRayNP, self.floorQueue)
+
+    def update(self, dt):
+        self.updateMovement(dt)
+        self.updateMouseLook(dt)
+        
+    def updateMovement(self, dt):
+        # Mouvement horizontal
+        x_movement = 0
+        y_movement = 0
+
+        if self.keyMap['forward']:
+            x_movement -= dt * self.moveSpeed * sin(degToRad(self.game.camera.getH()))
+            y_movement += dt * self.moveSpeed * cos(degToRad(self.game.camera.getH()))
+        if self.keyMap['backward']:
+            x_movement += dt * self.moveSpeed * sin(degToRad(self.game.camera.getH()))
+            y_movement -= dt * self.moveSpeed * cos(degToRad(self.game.camera.getH()))
+        if self.keyMap['left']:
+            x_movement -= dt * self.moveSpeed * cos(degToRad(self.game.camera.getH()))
+            y_movement -= dt * self.moveSpeed * sin(degToRad(self.game.camera.getH()))
+        if self.keyMap['right']:
+            x_movement += dt * self.moveSpeed * cos(degToRad(self.game.camera.getH()))
+            y_movement += dt * self.moveSpeed * sin(degToRad(self.game.camera.getH()))
+
+        # Saut et traversée
+        if self.keyMap['up'] and self.onGround:
+            self.zVel = self.jumpSpeed
+            self.onGround = False
+            
+        if self.keyMap['down']:
+            if hasattr(self, "playerCollider"):
+                self.playerCollider.node().setFromCollideMask(BitMask32.allOff())
+        else:
+            if hasattr(self, "playerCollider"):
+                self.playerCollider.node().setFromCollideMask(self.game.worldMask)
+
+        # Application mouvement
+        self.game.camera.setPos(
+            self.game.camera.getX() + x_movement,
+            self.game.camera.getY() + y_movement,
+            self.game.camera.getZ()
+        )
+
+        # Gravité
+        self.zVel += self.gravity * dt
+        newZ = self.game.camera.getZ() + self.zVel * dt
+
+        # Detection sol
+        if self.floorQueue.getNumEntries() > 0:
+            self.floorQueue.sortEntries()
+            entry = self.floorQueue.getEntry(0)
+            surfaceZ = entry.getSurfacePoint(self.game.render).getZ()
+            dist = self.game.camera.getZ() - surfaceZ
+            if dist <= 1.1 and self.zVel <= 0:
+                self.onGround = True
+                self.zVel = 0.0
+                newZ = surfaceZ + 1.0
+            else:
+                self.onGround = False
+
+        self.game.camera.setZ(newZ)
+
+    def updateMouseLook(self, dt):
+        if self.cameraSwingActivated:
+            if not self.game.win.getProperties().getForeground():
+                self.releaseMouse()
+            else:
+                wp = self.game.win.getProperties()
+                cx = int(wp.getXSize() / 2)
+                cy = int(wp.getYSize() / 2)
+
+                md = self.game.win.getPointer(0)
+                mouseX = md.getX()
+                mouseY = md.getY()
+
+                mouseChangeX = mouseX - cx
+                mouseChangeY = mouseY - cy
+
+                currentH = self.game.camera.getH()
+                currentP = self.game.camera.getP()
+
+                self.game.camera.setHpr(
+                    currentH - mouseChangeX * dt * self.cameraSwingFactor,
+                    min(90, max(-90, currentP - mouseChangeY * dt * self.cameraSwingFactor)),
+                    0
+                )
+                
+                self.game.win.movePointer(0, cx, cy)
+
+    def updateKeyMap(self, key, value):
+        self.keyMap[key] = value
+
+    def captureMouse(self):
+        self.cameraSwingActivated = True
+        properties = WindowProperties()
+        properties.setCursorHidden(True)
+        properties.setMouseMode(WindowProperties.M_confined)
+        self.game.win.requestProperties(properties)
+        
+        wp = self.game.win.getProperties()
+        self.game.win.movePointer(0, int(wp.getXSize() / 2), int(wp.getYSize() / 2))
+
+    def releaseMouse(self):
+        self.cameraSwingActivated = False
+        properties = WindowProperties()
+        properties.setCursorHidden(False)
+        properties.setMouseMode(WindowProperties.M_absolute)
+        self.game.win.requestProperties(properties)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+#programme
+import Player
+
+#librairie
 from math import pi, sin, cos 
 
+#librairie panda3d
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFile 
 from panda3d.core import DirectionalLight, AmbientLight
@@ -18,214 +23,32 @@ def degToRad(deg):
 class MyGame(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
-        # physique du joueur
-        self.zVel = 0.0
-        self.gravity = -20.0
-        self.jumpSpeed = 7.0
-        self.onGround = False
-        # mask utilisé pour les collisions "sol / blocs"
+        
         self.worldMask = BitMask32.bit(1)
+        self.cTrav = CollisionTraverser()
+        self.pusher = CollisionHandlerPusher()
+        self.floorQueue = CollisionHandlerQueue()
 
         self.loadModels()
         self.setupLights()
         self.generateTerrain()
-        self.setupCamera()
+        
+        # Créer le joueur après avoir initialisé les systèmes nécessaires
+        self.player = Player.Player(self)
+        
         self.setupSkybox()
-        self.captureMouse()
-        self.setupControls()
 
         taskMgr.add(self.update, "update")
 
     def update(self, task):
         dt = globalClock.getDt()
-
-        playerMoveSpeed = 10
-
-        x_movement = 0
-        y_movement = 0
-
-        if self.keyMap['forward']:
-            x_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
-            y_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
-        if self.keyMap['backward']:
-            x_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
-            y_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
-        if self.keyMap['left']:
-            x_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
-            y_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
-        if self.keyMap['right']:
-            x_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
-            y_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
-        # gestion saut / tomber
-        if self.keyMap['up'] and self.onGround:
-            # on ne peut sauter que si on est au sol
-            self.zVel = self.jumpSpeed
-            self.onGround = False
-        # si on maintient LShift, on veut passer à travers les blocs vers le bas :
-        if self.keyMap['down']:
-            # désactiver temporairement le collider "from" du joueur pour traverser
-            if hasattr(self, "playerCollider"):
-                self.playerCollider.node().setFromCollideMask(BitMask32.allOff())
-        else:
-            # réactiver
-            if hasattr(self, "playerCollider"):
-                self.playerCollider.node().setFromCollideMask(self.worldMask)
-
-        camera.setPos(
-            camera.getX() + x_movement,
-            camera.getY() + y_movement,
-            camera.getZ()
-        )
-
-        # lancer la traversée de collision pour mettre à jour self.rayQueue
+        
         if hasattr(self, "cTrav"):
             self.cTrav.traverse(render)
-            # ici tu peux lire self.rayQueue pour voir les collisions si besoin
-
-        # appliquer gravité et mouvement vertical
-        self.zVel += self.gravity * dt
-        newZ = self.camera.getZ() + self.zVel * dt
-
-        # lire la queue du rayon vers le bas pour détecter le sol
-        if hasattr(self, "floorQueue"):
-            if self.floorQueue.getNumEntries() > 0:
-                self.floorQueue.sortEntries()
-                entry = self.floorQueue.getEntry(0)
-                surfacePt = entry.getSurfacePoint(render)
-                surfaceZ = surfacePt.getZ()
-                # distance entre la caméra et la surface juste en dessous
-                dist = self.camera.getZ() - surfaceZ
-                # ajuster seuil en fonction de la "hauteur" du joueur (ici ~1.0)
-                groundThreshold = 1.1
-                if dist <= groundThreshold and self.zVel <= 0:
-                    # on est au sol : bloquer la chute et remettre la caméra sur le sol
-                    self.onGround = True
-                    self.zVel = 0.0
-                    newZ = surfaceZ + 1.0
-                else:
-                    self.onGround = False
-
-        # appliquer la nouvelle position Z
-        self.camera.setZ(newZ)
-
-
-        if self.cameraSwingActivated:
-            # si on perd le focus, relâcher la souris pour éviter blocage
-            if not self.win.getProperties().getForeground():
-                self.releaseMouse()
-            else:
-                wp = self.win.getProperties()
-                cx = int(wp.getXSize() / 2)
-                cy = int(wp.getYSize() / 2)
-
-                md = self.win.getPointer(0)
-                mouseX = md.getX()
-                mouseY = md.getY()
-
-                # Calculer le changement par rapport au centre
-                mouseChangeX = mouseX - cx
-                mouseChangeY = mouseY - cy
-
-                
-                self.cameraSwingFactor = 10
-
-                currentH = self.camera.getH()
-                currentP = self.camera.getP()
-
-                self.camera.setHpr(
-                    currentH - mouseChangeX * dt * self.cameraSwingFactor,
-                    min(90, max(-90, currentP - mouseChangeY * dt * self.cameraSwingFactor)),
-                    0
-                )
-                # recentrer la souris
-                self.win.movePointer(0, cx, cy)
-
+            
+        self.player.update(dt)
+        
         return task.cont
-
-    def setupControls(self):
-        self.keyMap = {
-            "forward": False,
-            "backward": False,
-            "left": False,
-            "right": False,
-            "up": False,
-            "down": False
-        }
-        self.accept("escape", self.releaseMouse)
-        self.accept("mouse1", self.captureMouse)
-
-        self.accept("z", self.updateKeyMap, ["forward", True])
-        self.accept("z-up", self.updateKeyMap, ["forward", False])
-        self.accept("s", self.updateKeyMap, ["backward", True])
-        self.accept("s-up", self.updateKeyMap, ["backward", False])
-        self.accept("q", self.updateKeyMap, ["left", True])
-        self.accept("q-up", self.updateKeyMap, ["left", False])
-        self.accept("d", self.updateKeyMap, ["right", True])
-        self.accept("d-up", self.updateKeyMap, ["right", False])
-        self.accept("space", self.updateKeyMap, ["up", True])
-        self.accept("space-up", self.updateKeyMap, ["up", False])
-        self.accept("lshift", self.updateKeyMap, ["down", True])
-        self.accept("lshift-up", self.updateKeyMap, ["down", False])
-
-    def updateKeyMap(self, key, value):
-        self.keyMap[key] = value
-
-    def captureMouse(self):
-        self.cameraSwingActivated = True
-        # mettre la souris cachée et en mode relatif, puis recentrer immédiatement
-        properties = WindowProperties()
-        properties.setCursorHidden(True)
-        properties.setMouseMode(WindowProperties.M_confined)  # ou M_confined pour garder le curseur dans la fenêtre
-        self.win.requestProperties(properties)
-
-        # recentrer la souris
-        wp = self.win.getProperties()
-        cx = int(wp.getXSize() / 2)
-        cy = int(wp.getYSize() / 2)
-        self.win.movePointer(0, cx, cy)
-
-        
-    def releaseMouse(self):
-        self.cameraSwingActivated = False
-        properties = WindowProperties()
-        properties.setCursorHidden(False)
-        properties.setMouseMode(WindowProperties.M_absolute)
-        self.win.requestProperties(properties)
-
-    def setupCamera(self):
-        self.disableMouse()
-        self.camera.setPos(0, 0, 3)
-        crosshairs = OnscreenImage(
-            image='crosshairs.png', 
-            pos=(0, 0, 0), 
-            scale=0.05
-        )
-        crosshairs.setTransparency(TransparencyAttrib.MAlpha)
-
-        self.cTrav = CollisionTraverser()
-        # collider "player" (utilisé par le Pusher)
-        self.pusher = CollisionHandlerPusher()
-        playerNode = CollisionNode('player-collider')
-        playerNode.addSolid(CollisionSphere(0, 0, 0, 0.5))
-        playerNode.setFromCollideMask(self.worldMask)
-        playerNode.setIntoCollideMask(BitMask32.allOff())
-        self.playerCollider = self.camera.attachNewNode(playerNode)
-        self.cTrav.addCollider(self.playerCollider, self.pusher)
-        self.pusher.addCollider(self.playerCollider, self.camera)
-
-        # rayon vers le bas pour détecter le sol
-        floorRay = CollisionRay(0, 0, 0, 0, 0, -1)
-        floorNode = CollisionNode('floor-ray')
-        floorNode.addSolid(floorRay)
-        floorNode.setFromCollideMask(self.worldMask)
-        floorNode.setIntoCollideMask(BitMask32.allOff())
-        self.floorRayNP = self.camera.attachNewNode(floorNode)
-        self.floorQueue = CollisionHandlerQueue()
-        self.cTrav.addCollider(self.floorRayNP, self.floorQueue)
- 
-        
-
         
     def setupSkybox(self):
         skybox = loader.loadModel('skybox/skybox.egg')
@@ -234,8 +57,6 @@ class MyGame(ShowBase):
         skybox.setDepthWrite(0)
         skybox.setLightOff()
         skybox.reparentTo(render)
-
-
 
     def generateTerrain(self):
         for z in range(10):
@@ -260,10 +81,10 @@ class MyGame(ShowBase):
                     collider.setPythonTag('owner', newBlockNode)
 
     def loadModels(self):
-        self.grassBlock = loader.loadModel('grass-block.glb')
-        self.dirtBlock = loader.loadModel('dirt-block.glb')
-        self.stoneBlock = loader.loadModel('stone-block.glb')
-        self.sandBlock = loader.loadModel('sand-block.glb')
+        self.grassBlock = loader.loadModel('model3d/grass-block.glb')
+        self.dirtBlock = loader.loadModel('model3d/dirt-block.glb')
+        self.stoneBlock = loader.loadModel('model3d/stone-block.glb')
+        self.sandBlock = loader.loadModel('model3d/sand-block.glb')
 
     def setupLights(self):
         mainLight = DirectionalLight('mainLight')
